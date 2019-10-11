6f2a2d6f53e6239f54ced571f2de3a484ce4726b
Kevin <koster.2001@gmail.com>
2019-10-11 14:15:27 +0200

    eo2

--- src/eindopdracht2/ConnectDB.java
+++ src/eindopdracht2/ConnectDB.java
@@ -4,20 +4,55 @@ import java.sql.*;
 
 public class ConnectDB {
 
-	private Connection con;
-	private Statement st;
-	private ResultSet rs;
+	public Connection con;
 	
-	public ConnectDB() {
+	public void createConnection(String databasename, String username, String passwoord) {
 		
-		try{
-			Class.forName("com.mysql.jbdc.Driver");
-			
-			
-		} catch(Exception ex) {
-			System.out.println("Error: " + ex);
+		try {
+			Class.forName("com.mysql.jdbc.Driver");
+			con = DriverManager.getConnection("jdbc:mysql://127.0.0.1:3306/" + databasename + username + password);
+			try {
+				if(!con.isClosed()) {
+					System.out.println("Connection succesful.");
+				}
+			} catch(SQLException e) {
+				System.out.println(e);
+			}
+		} catch(Exception e) {
+			System.out.println(e);
 		}
 		
 	}
 	
-}
+	public void getData() {
+		try {
+			if(!con.isClosed()) {
+				Statement statement = con.createStatement();
+				String query = "select * from namen";
+				ResultSet results = statement.executeQuery(query);
+				while(results.next()) {
+					PersoonLijst.listModel.addElement(results.getString(2) + " " + results.getString(3));
+				}
+			}
+			
+		}catch (Exception e) {
+				System.out.println(e);
+			}
+		}
+	
+	public void insertData(String voornaam, String achternaam) {
+		try {
+			if(!con.isClosed()) {
+				Statement statement = con.createStatement();
+				String query = String.format("insert into persoonlijst (Voornaam,Achternaam) values ('%s','%s')",voornaam,achternaam);
+				statement.executeUpdate(query);
+				System.out.println("Insert complete!");
+			}
+		}catch (Exception e) {
+			System.out.println(e);
+		}
+	}
+	
+	}
+	
+
--- src/eindopdracht2/PersoonLijst.java
+++ src/eindopdracht2/PersoonLijst.java
@@ -1,120 +1,75 @@
 package eindopdracht2;
 
-import java.awt.Color;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
 import javax.swing.DefaultListModel;
 import javax.swing.JButton;
 import javax.swing.JFrame;
-import javax.swing.JLabel;
 import javax.swing.JList;
+import javax.swing.JPanel;
+import javax.swing.JScrollPane;
 import javax.swing.JTextField;
-import javax.swing.SwingUtilities;
 
 public class PersoonLijst extends JFrame{
 	
-	public PersoonLijst() {
-		
-		//het setten van de windownaam.
-		super("Eclipse-Personen");
-		
-		//properties van de window.
-		setSize(500,500);
-		setLayout(null);
-		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		setVisible(true);
-		
-		//variabelen
-		JTextField voornaam = new JTextField();								// de text field voor voornaam
-		JTextField achternaam = new JTextField();							// de text field voor achternaam
-		JButton subBtn = new JButton("Toevoegen");							// de button om toe te voegen
-		JButton delBtn = new JButton("Reset");								// de button om te resetten
-		DefaultListModel<String> model = new DefaultListModel<>();
-		JList<String> PersoonLijst = new JList<>(model);
-		
-		//labels
-		JLabel laVoornaam = new JLabel();
-		laVoornaam.setText("Voornaam :");
-		JLabel laAchternaam = new JLabel();
-		laAchternaam.setText("Achternaam :");
-		JLabel output = new JLabel();
-		
-		//opmaak
-		voornaam.setBounds(110, 10, 100, 30);
-		laVoornaam.setBounds(10,10,100,30);
-		achternaam.setBounds(110, 50, 100, 30);
-		laAchternaam.setBounds(10,50,100,30);
-		subBtn.setBounds(10, 100, 100, 30);
-		delBtn.setBounds(130, 100, 100, 30);
-		output.setBounds(10, 150, 400, 20);
-		PersoonLijst.setBounds(10, 250, 460, 500);
-		
-		//tooltips
-		voornaam.setToolTipText("Voer hier je voornaam in.");
-		achternaam.setToolTipText("Voer hier je achternaam in.");
-		subBtn.setToolTipText("Voegt jouw naam toe aan de lijst.");
-		
-		//uitprinten van button, fields en labels.
-		add(laVoornaam);
-		add(voornaam);
-		add(laAchternaam);
-		add(achternaam);
-		add(output);
-		add(subBtn);
-		add(delBtn);
-		add(PersoonLijst);
-
-		
-		subBtn.addActionListener( new ActionListener() {
-			
-			public void actionPerformed(ActionEvent event) {
-				String message = voornaam.getText() + " " + achternaam.getText() + " is toegevoegd aan de lijst.";
-				String error = "Voor- en achternaam moeten ingevuld worden.";
-				
-				if (voornaam.getText().equals("") || achternaam.getText().equals("")) {
-					output.setText(error);
-					output.setForeground(Color.RED);
-				} else {
-					output.setText(message);
-					output.setForeground(Color.BLUE);
-					model.addElement(voornaam.getText() + " " + achternaam.getText());
-					voornaam.setText("");
-					achternaam.setText("");
-				}
-			}
-			
-		});
-		
-		delBtn.addActionListener( new ActionListener() {
-			
-			public void actionPerformed(ActionEvent event) {
-				String clearMsg = "Alle namen zijn uit de lijst gehaald.";
-				
-				output.setText(clearMsg);
-				output.setForeground(Color.BLUE);
-				voornaam.setText("");
-				achternaam.setText("");
-				model.clear();
-				
-			}
-			
-		});
-		
-	}
-	
+	public static DefaultListModel<String> listModel;
 	
 	public static void main(String[] args) {
+		ConnectDB database = new ConnectDB();
+		database.createConnection("persoonlijst", "root", "");
+		JFrame f = new JFrame("Persoonlijst");
+		f.setVisible(true);
+		f.setSize(500,500);
+		f.setDefaultCloseOperation(EXIT_ON_CLOSE);
 		
-		ConnectDB connect = new ConnectDB();
+		JPanel panel = new JPanel();
+		panel.setLayout(null);
+		f.add(panel);
 		
-		SwingUtilities.invokeLater(new Runnable() {
-			
-			public void run() {
-				new PersoonLijst();
+		JTextField voornaam = new JTextField(20);
+		voornaam.setBounds(0,0,200,30);
+		panel.add(voornaam);
+		
+		JTextField achternaam = new JTextField(20);
+		achternaam.setBounds(335,0,200,30);
+		panel.add(achternaam);
+		
+		listModel = new DefaultListModel<>();
+		
+		JList<String> list = new JList<>(listModel);
+		JScrollPane s = new JScrollPane(list);
+		s.setHorizontalScrollBarPolicy(JScrollPane.HORIZONTAL_SCROLLBAR_NEVER);
+		s.setVerticalScrollBarPolicy(JScrollPane.VERTICAL_SCROLLBAR_AS_NEEDED);
+		s.setBounds(150,120, 200, 100);
+		panel.add(s);
+		
+		JButton toevoeg = new JButton("Persoon toevoegen");
+		toevoeg.setBounds(150, 250, 200, 50);
+		toevoeg.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				if(e.getSource() == toevoeg) {
+					database.insertData(voornaam.getText(), achternaam.getText());
 				}
-			
+			}
 		});
+		panel.add(toevoeg);
+		JButton selectall = new JButton("Krijg alle personen");
+		selectall.setBounds(150,300,200,50);
+		selectall.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				if(e.getSource() == selectall) {
+					listModel.clear();
+					database.getData();
+				}
+			}
+		});
+		panel.add(selectall);
+		f.revalidate();
+		f.setVisible(true);
+		f.setResizable(false);
+	}
+		
 	}
 	
 }
