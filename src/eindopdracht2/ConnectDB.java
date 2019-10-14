b2962940e6c8fbf920d4a02393e4ee1875ebdde4
JesperBorst <125890@student.drenthecollege.nl>
2019-10-14 09:31:54 +0200

    commit

--- src/eindopdracht2/ConnectDB.java
+++ src/eindopdracht2/ConnectDB.java
@@ -9,7 +9,7 @@ public class ConnectDB {
 	public void createConnection(String databasename, String username, String password) {
 		
 		try {
-			Class.forName("com.mysql.jdbc.Driver");
+			Class.forName("com.mysql.cj.jdbc.Driver");
 			con = DriverManager.getConnection("jdbc:mysql://127.0.0.1:3306/" + databasename + username + password);
 			try {
 				if(!con.isClosed()) {
